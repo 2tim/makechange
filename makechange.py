0bfd0a5aae9d7ab9245a1d34c141d262567d05ed
Kevin Evans <kevin.evans@rigaku.com>
2014-06-19 10:26:23 -0500

    Initial commit

---
+++ makechange.py
@@ -0,0 +1,26 @@
+__author__ = 'kevev'
+
+def makechange(amount, denom=10):
+    """
+
+    :param amount:
+    :param denom:
+    """
+    global count
+    denoms = [10, 5, 1]
+    if amount < 1:
+        return
+    if denom == 1:
+        print  str(amount) + " for denom " + str(denom) + " in " + str(amount)
+        count+=1
+        return
+    for i in range(amount / denom, -1, -1):
+        print str(i) + " for denom " + str(denom) + " in " + str(amount)
+        if amount - i * denom > 0:
+            makechange(amount - i * denom, denoms[denoms.index(denom) + 1])
+        else:
+            count+=1
+
+count = 0
+makechange(11, denom=10)
+print str(count) + " unique combos"
