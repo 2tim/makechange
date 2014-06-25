5e4d7f45d98115226ca64a52784b59d1703b69d2
Kevin Evans <kevin.evans@rigaku.com>
2014-06-25 09:34:52 -0500

    changed to xrange

--- makechange.py
+++ makechange.py
@@ -14,7 +14,7 @@ def makechange(amount, denom=10):
         print  str(amount) + " for denom " + str(denom) + " in " + str(amount)
         count+=1
         return
-    for i in range(amount / denom, -1, -1):
+    for i in xrange(amount / denom, -1, -1):
         print str(i) + " for denom " + str(denom) + " in " + str(amount)
         if amount - i * denom > 0:
             makechange(amount - i * denom, denoms[denoms.index(denom) + 1])
